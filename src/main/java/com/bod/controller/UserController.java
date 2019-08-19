9eef09e9d9d8a69a26c09d6ead35b7369b5e6109
Bohdan Pukhalskyi <bohdanpukhalky@ukr.net>
2019-08-20 02:06:44 +0300

    add username output in header

--- src/main/java/com/bod/controller/UserController.java
+++ src/main/java/com/bod/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.bod.controller;
 
-import com.bod.domain.LifeStyle;
 import com.bod.domain.User;
 import com.bod.repository.UserRepository;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -17,7 +19,20 @@ public class UserController {
     UserRepository userRepository;
 
     @GetMapping("/user")
-    public String getContent(){
+    public String getContent(Model model){
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String username;
+
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails)principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+
+        User user = userRepository.findByName(username);
+
+        model.addAttribute(user);
         return "user";
     }
 
